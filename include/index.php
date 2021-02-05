f352179053387acf37e85e1b912b50b91e36fe8b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-02-05 16:17:34 +0600

    add include file

---
+++ include/index.php
@@ -0,0 +1,338 @@
+       <!-- header section started -->
+       <?php
+       include('include/header.php');
+       ?>
+       <!-- header section end -->
+
+        <!-- main-area -->
+        <main>
+
+            <!-- banner section started -->
+            <?php
+            include('include/banner.php')
+            ?>
+            <!-- banner section end -->
+            <!-- about-area-->
+            <section id="about" class="about-area primary-bg pt-120 pb-120">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="about-img">
+                                <img src="img/banner/banner_img2.png" title="me-01" alt="me-01">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 pr-90">
+                            <div class="section-title mb-25">
+                                <span>Introduction</span>
+                                <h2>About Me</h2>
+                            </div>
+                            <div class="about-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, sed repudiandae odit deserunt, quas
+                                    quibusdam necessitatibus nesciunt eligendi esse sit non reprehenderit quisquam asperiores maxime
+                                    blanditiis culpa vitae velit. Numquam!</p>
+                                <h3>Education:</h3>
+                            </div>
+                            <!-- Education Item -->
+                            <div class="education">
+                                <div class="year">2020</div>
+                                <div class="line"></div>
+                                <div class="location">
+                                    <span>PHD of Interaction Design &amp; Animation</span>
+                                    <div class="progressWrapper">
+                                        <div class="progress">
+                                            <div class="progress-bar wow slideInLefts" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 65%;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Education Item -->
+                            <!-- Education Item -->
+                            <div class="education">
+                                <div class="year">2016</div>
+                                <div class="line"></div>
+                                <div class="location">
+                                    <span>Master of Database Administration</span>
+                                    <div class="progressWrapper">
+                                        <div class="progress">
+                                            <div class="progress-bar wow slideInLefts" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Education Item -->
+                            <!-- Education Item -->
+                            <div class="education">
+                                <div class="year">2010</div>
+                                <div class="line"></div>
+                                <div class="location">
+                                    <span>Bachelor of Computer Engineering</span>
+                                    <div class="progressWrapper">
+                                        <div class="progress">
+                                            <div class="progress-bar wow slideInLefts" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0"
+                                                aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Education Item -->
+                            <!-- Education Item -->
+                            <div class="education">
+                                <div class="year">2005</div>
+                                <div class="line"></div>
+                                <div class="location">
+                                    <span>Diploma of Computer</span>
+                                    <div class="progressWrapper">
+                                        <div class="progress">
+                                            <div class="progress-bar wow slideInLefts" data-wow-delay="0.2s" data-wow-duration="2s" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0"
+                                                aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Education Item -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- about-area-end -->
+
+            <!-- Services-area -->
+            <section id="service" class="services-area pt-120 pb-50">
+				<div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-6 col-lg-8">
+                            <div class="section-title text-center mb-70">
+                                <span>WHAT WE DO</span>
+                                <h2>Services and Solutions</h2>
+                            </div>
+                        </div>
+                    </div>
+					<div class="row">
+						<div class="col-lg-4 col-md-6">
+							<div class="icon_box_01 wow fadeInLeft" data-wow-delay="0.2s">
+                                <i class="fab fa-react"></i>
+								<h3>Creative Design</h3>
+								<p>
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indust.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="icon_box_01 wow fadeInLeft" data-wow-delay="0.4s">
+								<i class="fab fa-free-code-camp"></i>
+								<h3>Unlimited Features</h3>
+								<p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indust.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="icon_box_01 wow fadeInLeft" data-wow-delay="0.6s">
+                                <i class="fal fa-desktop"></i>
+								<h3>Ultra Responsive</h3>
+								<p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indust.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="icon_box_01 wow fadeInLeft" data-wow-delay="0.2s">
+                                <i class="fal fa-lightbulb-on"></i>
+								<h3>Creative Ideas</h3>
+								<p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indust.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="icon_box_01 wow fadeInLeft" data-wow-delay="0.4s">
+                                <i class="fal fa-edit"></i>
+								<h3>Easy Customization</h3>
+								<p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indust.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="icon_box_01 wow fadeInLeft" data-wow-delay="0.6s">
+                                <i class="fal fa-headset"></i>
+								<h3>Supper Support</h3>
+								<p>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indust.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+            <!-- Services-area-end -->
+
+            <!-- Portfolios-area -->
+            <section id="portfolio" class="portfolio-area primary-bg pt-120 pb-90">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-6 col-lg-8">
+                            <div class="section-title text-center mb-70">
+                                <span>Portfolio Showcase</span>
+                                <h2>My Recent Best Works</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 pitem">
+                            <div class="speaker-box">
+								<div class="speaker-thumb">
+									<img src="img/images/1.jpg" alt="img">
+								</div>
+								<div class="speaker-overlay">
+									<span>Design</span>
+									<h4><a href="portfolio-single.html">Hamble Triangle</a></h4>
+									<a href="portfolio-single.html" class="arrow-btn">More information <span></span></a>
+								</div>
+							</div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 pitem">
+                            <div class="speaker-box">
+								<div class="speaker-thumb">
+									<img src="img/images/2.jpg" alt="img">
+								</div>
+								<div class="speaker-overlay">
+									<span>Video</span>
+									<h4><a href="portfolio-single.html">Dark Beauty</a></h4>
+									<a href="portfolio-single.html" class="arrow-btn">More information <span></span></a>
+								</div>
+							</div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 pitem">
+                            <div class="speaker-box">
+								<div class="speaker-thumb">
+									<img src="img/images/3.jpg" alt="img">
+								</div>
+								<div class="speaker-overlay">
+									<span>Audio</span>
+									<h4><a href="portfolio-single.html">Gilroy Limbo.</a></h4>
+									<a href="portfolio-single.html" class="arrow-btn">More information <span></span></a>
+								</div>
+							</div>
+                        </div>
+						<div class="col-lg-4 col-md-6 pitem">
+                            <div class="speaker-box">
+								<div class="speaker-thumb">
+									<img src="img/images/4.jpg" alt="img">
+								</div>
+								<div class="speaker-overlay">
+									<span>Design</span>
+									<h4><a href="portfolio-single.html">Ipsum which</a></h4>
+									<a href="portfolio-single.html" class="arrow-btn">More information <span></span></a>
+								</div>
+							</div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 pitem">
+                            <div class="speaker-box">
+								<div class="speaker-thumb">
+									<img src="img/images/5.jpg" alt="img">
+								</div>
+								<div class="speaker-overlay">
+									<span>Creative</span>
+									<h4><a href="portfolio-single.html">Eiusmod tempor</a></h4>
+									<a href="portfolio-single.html" class="arrow-btn">More information <span></span></a>
+								</div>
+							</div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 pitem">
+                            <div class="speaker-box">
+								<div class="speaker-thumb">
+									<img src="img/images/6.jpg" alt="img">
+								</div>
+								<div class="speaker-overlay">
+									<span>UX/UI</span>
+									<h4><a href="portfolio-single.html">again there</a></h4>
+									<a href="portfolio-single.html" class="arrow-btn">More information <span></span></a>
+								</div>
+							</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- services-area-end -->
+
+
+            <!-- fact-area -->
+            <section class="fact-area">
+                <div class="container">
+                    <div class="fact-wrap">
+                        <div class="row justify-content-between">
+                            <div class="col-xl-2 col-lg-3 col-sm-6">
+                                <div class="fact-box text-center mb-50">
+                                    <div class="fact-icon">
+                                        <i class="flaticon-award"></i>
+                                    </div>
+                                    <div class="fact-content">
+                                        <h2><span class="count">245</span></h2>
+                                        <span>Feature Item</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-2 col-lg-3 col-sm-6">
+                                <div class="fact-box text-center mb-50">
+                                    <div class="fact-icon">
+                                        <i class="flaticon-like"></i>
+                                    </div>
+                                    <div class="fact-content">
+                                        <h2><span class="count">345</span></h2>
+                                        <span>Active Products</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-2 col-lg-3 col-sm-6">
+                                <div class="fact-box text-center mb-50">
+                                    <div class="fact-icon">
+                                        <i class="flaticon-event"></i>
+                                    </div>
+                                    <div class="fact-content">
+                                        <h2><span class="count">39</span></h2>
+                                        <span>Year Experience</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-2 col-lg-3 col-sm-6">
+                                <div class="fact-box text-center mb-50">
+                                    <div class="fact-icon">
+                                        <i class="flaticon-woman"></i>
+                                    </div>
+                                    <div class="fact-content">
+                                        <h2><span class="count">3</span>k</h2>
+                                        <span>Our Clients</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- fact-area-end -->
+
+          <!-- testimonial section started -->
+          <?php
+          include('include/testimonial.php');
+          ?>
+          <!-- testimonial section end -->
+            <!-- brand section started -->
+            <?php
+            include('include/brand.php');
+            ?>
+            <!-- brand section end -->
+          <!-- contact section started -->
+        <?php
+        include('include/contact.php');
+        ?>
+          <!-- contact section end -->
+
+        </main>
+        <!-- main-area-end -->
+
+      <!-- footer section started -->
+      <?php
+      include('include/footer.php');
+      ?>
+      <!-- footer section end -->
